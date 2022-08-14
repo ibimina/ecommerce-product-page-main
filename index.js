3ac0197e3282ef25cb268da99b46d575d8772573
ibimina <96787210+ibimina@users.noreply.github.com>
2022-08-14 22:57:51 +0100

    code refactoring

--- index.js
+++ index.js
@@ -1,281 +1,206 @@
 const mobileToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const checkCart = document.querySelector(".checkcart");
+const numberCart = document.querySelector(".qua");
+const emp = document.querySelector(".show-cart");
+const nos = document.querySelector(".quantity");
+const sneakerCon = document.querySelector(".new");
+const displaypic = document.querySelector(".displaypic");
+const picDiv = document.querySelector(".web");
 
 mobileToggle.addEventListener("click", (e) => {
   e.preventDefault();
   const visiblity = primaryNav.getAttribute("data-visible");
   if (visiblity === "false") {
     primaryNav.setAttribute("data-visible", true);
-
     mobileToggle.setAttribute("aria-expanded", true);
   } else {
     primaryNav.setAttribute("data-visible", false);
     mobileToggle.setAttribute("aria-expanded", false);
   }
 });
-
+let changeImage = document.querySelectorAll(".oo");
 const imageSlide = document.querySelectorAll(".product");
 const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
 let current = 0;
 
-function reset() {
-  for (let i = 0; i < imageSlide.length; i++) {
-    imageSlide[i].style.display = "none";
+function reset(image) {
+  for (let i = 0; i < image.length; i++) {
+    image[i].style.display = "none";
   }
 }
 
-function firstSlide() {
-  reset();
-  imageSlide[0].style.display = "block";
-}
-function leftSlide() {
-  reset();
-  imageSlide[current - 1].style.display = "block";
+reset(imageSlide);
+reset(changeImage);
+
+imageSlide[0].style.display = "block";
+changeImage[0].style.display = "block";
+
+function leftSlide(image) {
+  reset(image);
+  if (current === 0) {
+    current = image.length;
+  }
+  image[current - 1].style.display = "block";
   current--;
-  //imageSlide[0].style.display = "block";
 }
-function rightSlide() {
-  reset();
-  imageSlide[current + 1].style.display = "block";
+function rightSlide(image) {
+  reset(image);
+  if (current === image.length - 1) {
+    current = -1;
+  }
+  image[current + 1].style.display = "block";
   current++;
-  //imageSlide[0].style.display = "block";
 }
 
 leftArrow.addEventListener("click", function () {
-  if (current === 0) {
-    current = imageSlide.length;
-  }
-  leftSlide();
+  leftSlide(imageSlide);
 });
 
 rightArrow.addEventListener("click", function () {
-  if (current === imageSlide.length - 1) {
-    current = -1;
-  }
-  rightSlide();
-});
-firstSlide();
-//reset()
-const nos = document.querySelector(".quantity");
-const minus = document.querySelector(".minus");
-const plus = document.querySelector(".plus");
-
-minus.addEventListener("click", function () {
-  if (nos.textContent > 0) {
-    let current = Number(nos.textContent) - 1;
-    nos.textContent = current;
-  }
+  rightSlide(imageSlide);
 });
 
-plus.addEventListener("click", function () {
-  let current = Number(nos.textContent) + 1;
-  nos.textContent = current;
-});
-
-const addTo = document.querySelector(".addcart");
-
-addTo.addEventListener("click", (e) => {
-  let addProduct = e.target;
-  if (nos.textContent > 0) {
-    numberCart.textContent = nos.textContent;
+sneakerCon.addEventListener("click", (e) => {
+  try {
+    let btn = e.target;
+    if (btn.className === "plus") {
+      let prevsEl = btn.previousElementSibling;
+      prevsEl.textContent++;
+    } else if (btn.className === "minus") {
+      let nextEl = btn.nextElementSibling;
+      if (nextEl.textContent > 0) {
+        nextEl.textContent--;
+      }
+    } else if (btn.classList.contains("addcart")) {
+      console.log(btn);
+      if (nos.textContent > 0) {
+        console.log(nos.textContent);
+        console.log(numberCart);
+        numberCart.textContent = nos.textContent;
+        console.log(numberCart);
+      }
+    }
+  } catch (error) {
+    console.log(`error: ${error.message}`);
   }
 });
-
-const checkCart = document.querySelector(".checkcart");
-const numberCart = document.querySelector(".qua");
-const emp = document.querySelector(".empty");
-const not = document.querySelector(".cartcontents");
-let quant = document.querySelector('.productqua')
-let quantSum = document.querySelector(".producttotal");
-
-
-console.log(not);
-function emptyCart() {
-//   
-emp.style.display = 'block'
-}
-
-function notEmpty() {
- quant.textContent = numberCart.textContent
-
- quantSum.textContent = ` $${Number(numberCart.textContent) * 125}.00`
-
- not.style.display = "block";
-}
 
 emp.addEventListener("click", (e) => {
   let clear = e.target;
   if (clear.classList.contains("closecart")) {
-
     emp.style.display = "none";
- 
-  }
-});
-not.addEventListener("click", (e) => {
-  let clear = e.target;
-
-  if (clear.classList.contains("closecart")) {
- 
-     not.style.display = "none";
   } else if (clear.classList.contains("trash")) {
-  
-    numberCart.textContent=''
-     not.style.display = "none";
+    numberCart.textContent = "";
+    emp.style.display = "none";
   }
-
-
 });
 
-function see() {
-  checkCart.addEventListener("click", (e) => {
-    let check = e.target;
-
-    let checkQua = check.previousElementSibling;
-    
-    if (checkQua.textContent === "") {
-      // console.log("b");
-      emptyCart();
-    } else if (checkQua.textContent >= 0) {
-      notEmpty();
- 
-    }
-  });
+function see(e) {
+  console.log(e.target);
+  let checkQua = e.target.previousElementSibling;
+  if (checkQua.textContent === "") {
+    // emp.classList.add("empty");
+    emp.style.display = "block";
+    emp.innerHTML = `
+        <h5>Cart</h5>
+         <img src="images/icon-close.svg" alt="close cart" class="closecart" />
+       <div class="con-em">
+        <p class="empty-cart">Your cart is empty</p>
+        </div>
+       `;
+  } else if (checkQua.textContent >= 0) {
+    // emp.classList.add("cartcontents");
+    emp.style.display = "block";
+    emp.innerHTML = `<h5>Cart</h5>
+          <img src="images/icon-close.svg" alt="close image " class="closecart" />
+        <hr />
+       
+        <div class="cartcontent">
+          <div class="desc">
+            <img
+              src="images/image-product-1-thumbnail.jpg"
+              alt="image"
+              class="smal"/>
+            </div>
+          <div>
+              <p class="name">Autumn limited Edition</p>
+              <p class="total">
+                $125.00 x <span class="productqua">${
+                  numberCart.textContent
+                }</span>
+                <span class="producttotal"> $${
+                  Number(numberCart.textContent) * 125
+                }.00</span>
+              </p>
+           </div>
+               <img src="images/icon-delete.svg" alt="image" class="trash" />
+            </div>
+           <div class="cheq">
+         <button class="checkout">checkout</button>
+           </div>
+            `;
+  }
 }
-see();
 
-const displaypic = document.querySelector(".displaypic");
-const picDiv = document.querySelector(".web");
+checkCart.addEventListener("click", (e) => {
+  see(e);
+});
 
 picDiv.addEventListener("click", (e) => {
-
   let somw = e.target;
-
+  console.log(somw);
   if (somw.classList.contains("thumbone")) {
     displaypic.innerHTML = `<img src="images/image-product-1.jpg" alt="" />`;
-    
   } else if (somw.classList.contains("thumbtwo")) {
     displaypic.innerHTML = `<img src="images/image-product-2.jpg" alt="" />`;
-    
   } else if (somw.classList.contains("thumbthree")) {
     displaypic.innerHTML = `<img src="images/image-product-3.jpg" alt="" />`;
-  
   } else if (somw.classList.contains("thumbfour")) {
     displaypic.innerHTML = `<img src="images/image-product-4.jpg" alt="" />`;
-  
   }
 });
 const pictu = document.querySelector(".wrapper");
-const pictur = document.querySelector(".ss");
-
-function display(ret) {
-  pictur.src = ret.src;
-  console.log(pictur);
-}
 
 displaypic.addEventListener("click", (e) => {
-  console.log(e.target);
-  let ret = e.target;
   pictu.style.display = "block";
-  display(ret);
 });
-let clickk;
+
 const btn = document.querySelector(".wrapper");
 
 btn.addEventListener("click", (e) => {
   let click = e.target;
- 
-  let clickParent = click.parentElement.parentElement.previousElementSibling.children[0];
-  // console.log(clickParent)
+  let clickParent =
+    click.parentElement.parentElement.previousElementSibling.children[0];
+
   if (click.classList.contains("lightone")) {
-    // clickParent.style.display='block'
-     resete();
+    reset(changeImage);
     clickParent.innerHTML = `<img src="images/image-product-1.jpg" alt="" />`;
-   
-    // console.log("ab");
   } else if (click.classList.contains("lighttwo")) {
-    resete();
-    // clickParent.style.display='block'
+    reset(changeImage);
     clickParent.innerHTML = `<img src="images/image-product-2.jpg" alt="" />`;
-    
-    // console.log("b");
   } else if (click.classList.contains("lightthree")) {
-  resete();
-    // clickParent.style.display='block'
+    reset(changeImage);
     clickParent.innerHTML = `<img src="images/image-product-3.jpg" alt="" />`;
-      
-    // console.log("b");
   } else if (click.classList.contains("lightfour")) {
- resete();
-    // clickParent.style.display='block'
+    reset(changeImage);
     clickParent.innerHTML = `<img src="images/image-product-4.jpg" alt="" />`;
-       
-    console.log("b");
+  } else if (click.classList.contains("closebox")) {
+    btn.style.display = "none";
   }
 });
-
-const cl = document.querySelector(".closebox");
-function clo() {
-  cl.addEventListener("click", (e) => {
-    btn.style.display = "none";
-  });
-}
-clo();
-
-let changeImage = document.querySelectorAll(".oo");
-
-let curr = 0;
-function resete() {
-  for (let i = 0; i < changeImage.length; i++) {
-    changeImage[i].style.display = "none";
-  }
-}
-function lide() {
-  resete();
-  changeImage[0].style.display = "block";
-}
-function leftSlidee() {
-  resete();
-  changeImage[curr - 1].style.display = "block";
-  curr--;
-  //imageSlide[0].style.display = "block";
-}
-function rightlide() {
-  resete();
-  changeImage[curr + 1].style.display = "block";
-  curr++;
-  //imageSlide[0].style.display = "block";
-}
 
 let lefty = document.querySelector(".leftu");
 let rigty = document.querySelector(".rightu");
 let inlefty = document.querySelector(".so");
 
-function redo() {
-  lefty.addEventListener("click", function () {
-    if (curr === 0) {
-      curr = changeImage.length;
-      lide();
-    }
+lefty.addEventListener("click", function () {
+  leftSlide(changeImage);
+  inlefty.innerHTML = "";
+});
 
-    leftSlidee();
-    inlefty.innerHTML = "";
-  });
-}
-
-redo();
-
-function redoi() {
-  rigty.addEventListener("click", function () {
-    if (curr === changeImage.length - 1) {
-      curr = -1;
-         lide();
-    }
-    rightlide();
-    inlefty.innerHTML = "";
-  });
-}
-
-redoi();
-
-
+rigty.addEventListener("click", function () {
+  rightSlide(changeImage);
+  inlefty.innerHTML = "";
+});
